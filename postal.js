9be00d2ad5b100cec9a2021599d7bf838f48650d
Heather Hanks <38661534+hbhanks@users.noreply.github.com>
2020-03-06 16:34:56 -0700

    Adding currency format

--- postal.js
+++ postal.js
@@ -3,174 +3,182 @@ function getAmount(type, weight)
    var amount = 0.0;
    amount.toFixed(2)
 
-   if (type == "Letters (Stamped)")
+   if(weight > 0)
    {
-      if(weight <= 1.0)
+
+      if (type == "Letters (Stamped)")
       {
-         amount = 0.55;
+         if(weight <= 1.0)
+         {
+            amount = 0.55;
+         }
+         else if(weight <= 2.0)
+         {
+            amount = 0.70;
+         }
+         else if(weight <= 3.0)
+         {
+            amount = 0.85;
+         }
+         else if(weight <= 3.5)
+         {
+            amount = 1.0;
+         }
+         else
+         {
+            amount = "unknown";
+         }
       }
-      else if(weight <= 2.0)
+      else  if(type == "Letters (Metered)")
       {
-         amount = 0.70;
+         if(weight <= 1.0)
+         {
+            amount = 0.50;
+         }
+         else if(weight <= 2.0)
+         {
+            amount = 0.65;
+         }
+         else if(weight <= 3.0)
+         {
+            amount = 0.80;
+         }
+         else if(weight <= 3.5)
+         {
+            amount = 0.95;
+         }
+         else
+         {
+            amount = "unknown";
+         }
       }
-      else if(weight <= 3.0)
+      else if(type == "Large Envelopes (Flats)")
       {
-         amount = 0.85;
+         if(weight <= 1.0)
+         {
+            amount = 1.00;
+         }
+         else if(weight <= 2.0)
+         {
+            amount = 1.20;
+         }
+         else if(weight <= 3.0)
+         {
+            amount = 1.40;
+         }
+         else if(weight <= 4.0)
+         {
+            amount = 1.60;
+         }
+         else if(weight <= 5.0)
+         {
+            amount = 1.80;
+         }
+         else if(weight <= 6.0)
+         {
+            amount = 2.00;
+         }
+         else if(weight <= 7.0)
+         {
+            amount = 2.20;
+         }
+         else if(weight <= 8.0)
+         {
+            amount = 2.40;
+         }
+         else if(weight <= 9.0)
+         {
+            amount = 2.60;
+         }
+         else if(weight <= 10.0)
+         {
+            amount = 2.80;
+         }
+         else if(weight <= 11.0)
+         {
+            amount = 3.00;
+         }
+         else if(weight <= 12.0)
+         {
+            amount = 3.20;
+         }
+         else if(weight <= 13.0)
+         {
+            amount = 3.40;
+         }
+         else
+         {
+            amount = "unknown";
+         }
       }
-      else if(weight <= 3.5)
+      else if(type == "First-Class Package Service—Retail")
       {
-         amount = 1.0;
+         if(weight <= 1.0)
+         {
+            amount = 3.80;
+         }
+         else if(weight <= 2.0)
+         {
+            amount = 3.80;
+         }
+         else if(weight <= 3.0)
+         {
+            amount = 3.80;
+         }
+         else if(weight <= 4.0)
+         {
+            amount = 3.80;
+         }
+         else if(weight <= 5.0)
+         {
+            amount = 4.60;
+         }
+         else if(weight <= 6.0)
+         {
+            amount = 4.60;
+         }
+         else if(weight <= 7.0)
+         {
+            amount = 4.60;
+         }
+         else if(weight <= 8.0)
+         {
+            amount = 4.60;
+         }
+         else if(weight <= 9.0)
+         {
+            amount = 5.30;
+         }
+         else if(weight <= 10.0)
+         {
+            amount = 5.30;
+         }
+         else if(weight <= 11.0)
+         {
+            amount = 5.30;
+         }
+         else if(weight <= 12.0)
+         {
+            amount = 5.30;
+         }
+         else if(weight <= 13.0)
+         {
+            amount = 5.90;
+         }
+         else
+         {
+            amount = "unknown";
+         }
       }
       else
       {
          amount = "unknown";
       }
-   }
-   else  if(type == "Letters (Metered)")
-   {
-      if(weight <= 1.0)
+      if(amount != "unknown")
       {
-         amount = 0.50;
+         amount.toFixed(2);
+         amount = "$" + amount;
       }
-      else if(weight <= 2.0)
-      {
-         amount = 0.65;
-      }
-      else if(weight <= 3.0)
-      {
-         amount = 0.80;
-      }
-      else if(weight <= 3.5)
-      {
-         amount = 0.95;
-      }
-      else
-      {
-         amount = "unknown";
-      }
-   }
-   else if(type == "Large Envelopes (Flats)")
-   {
-      if(weight <= 1.0)
-      {
-         amount = 1.00;
-      }
-      else if(weight <= 2.0)
-      {
-         amount = 1.20;
-      }
-      else if(weight <= 3.0)
-      {
-         amount = 1.40;
-      }
-      else if(weight <= 4.0)
-      {
-         amount = 1.60;
-      }
-      else if(weight <= 5.0)
-      {
-         amount = 1.80;
-      }
-      else if(weight <= 6.0)
-      {
-         amount = 2.00;
-      }
-      else if(weight <= 7.0)
-      {
-         amount = 2.20;
-      }
-      else if(weight <= 8.0)
-      {
-         amount = 2.40;
-      }
-      else if(weight <= 9.0)
-      {
-         amount = 2.60;
-      }
-      else if(weight <= 10.0)
-      {
-         amount = 2.80;
-      }
-      else if(weight <= 11.0)
-      {
-         amount = 3.00;
-      }
-      else if(weight <= 12.0)
-      {
-         amount = 3.20;
-      }
-      else if(weight <= 13.0)
-      {
-         amount = 3.40;
-      }
-      else
-      {
-         amount = "unknown";
-      }
-   }
-   else if(type == "First-Class Package Service—Retail")
-   {
-      if(weight <= 1.0)
-      {
-         amount = 3.80;
-      }
-      else if(weight <= 2.0)
-      {
-         amount = 3.80;
-      }
-      else if(weight <= 3.0)
-      {
-         amount = 3.80;
-      }
-      else if(weight <= 4.0)
-      {
-         amount = 3.80;
-      }
-      else if(weight <= 5.0)
-      {
-         amount = 4.60;
-      }
-      else if(weight <= 6.0)
-      {
-         amount = 4.60;
-      }
-      else if(weight <= 7.0)
-      {
-         amount = 4.60;
-      }
-      else if(weight <= 8.0)
-      {
-         amount = 4.60;
-      }
-      else if(weight <= 9.0)
-      {
-         amount = 5.30;
-      }
-      else if(weight <= 10.0)
-      {
-         amount = 5.30;
-      }
-      else if(weight <= 11.0)
-      {
-         amount = 5.30;
-      }
-      else if(weight <= 12.0)
-      {
-         amount = 5.30;
-      }
-      else if(weight <= 13.0)
-      {
-         amount = 5.90;
-      }
-      else
-      {
-         amount = "unknown";
-      }
-   }
-   if(amount != "unknown")
-   {
-      amount.toFixed(2);
-      amount = "$" + amount;
    }
 
    return amount;
